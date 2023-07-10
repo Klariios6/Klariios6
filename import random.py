c92e4ef1c1b6e2a036aa6bfd53a897994eb1f7d6
Kei Larios <katie.melissa345@gmail.com>
2023-07-10 07:31:20 -0700

    make def

--- import random.py
+++ import random.py
@@ -1,5 +1,9 @@
-import random 
+import random
+num1= random.randint(1,10)
+num2= random.randint(1,10)
+operator= random.choice(['+','-','*'])
+operation= int({num1)
+question= input(f' what is the value of this operation {num1} {operator} {num2} ?')
+answer= int(question)
 
-num1= randint(1,10)
-num2=randint(1,10)
-print(num1*num2)
+print(if answer==operation 'yes good job' else print'wrong')
