1f26850e58b0b5b4e2a840c1ac5a2b28ff491cad
kei larios <katie.melissa345@gmail.com>
2023-07-13 11:23:39 -0400

    trying to use elif for the first time

---
+++ sheet 4.py
@@ -0,0 +1,34 @@
+from random import randint as r
+x= r(1,1000)
+question=input(f'what number im i thinking of?')
+answer=int(question)
+
+
+
+#print("great" if answer== x else print("wrong"))
+
+while (answer, x):
+   if answer==x: print('wow maybe the easy option is a good one after all') 
+   elif answer !=x: print("try next time")
+   break
+()
+
+#else: print(' Whoops, your value is lower')
+#elif answer>x: print (' number is lower than the value ')()
+
+#x <1000:
+    #print( 'x is lower than the number you typed')
+
+    
+    
+
+#answer !=  100
+
+
+#while True: 
+   # x= r(1,1000)
+   # print(x)
+   # if x < 100 
+   # print( 'go lower')
+   
+
