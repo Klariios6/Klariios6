82d3500f601150adb74733252fc6bec60c568c0d
kei larios <katie.melissa345@gmail.com>
2023-07-13 11:50:03 -0400

    changed answer

--- sheet 4.py
+++ sheet 4.py
@@ -1,17 +1,23 @@
 from random import randint as r
 x= r(1,1000)
-question=input(f'what number im i thinking of?')
-answer=int(question)
-
-
+question= (input('what is your guess'))
+answer= int(question)
+tries=0
 
 #print("great" if answer== x else print("wrong"))
 
-while (answer, x):
-   if answer==x: print('wow maybe the easy option is a good one after all') 
-   elif answer !=x: print("try next time")
-   break
-()
+while True:
+    tries += 1
+
+    if answer> x: print(' go lower')
+    elif answer< x: print('go higher')
+    else: print('yummy')
+
+    play_again=input("play again yes or no? ")
+    if play_again.lower() != "no": 
+        break
+    ()
+
 
 #else: print(' Whoops, your value is lower')
 #elif answer>x: print (' number is lower than the value ')()
