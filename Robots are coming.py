0412e7552830f17fe674ccdd50446fc47fe7e596
kei larios <katie.melissa345@gmail.com>
2023-07-18 10:38:26 -0400

    move func was successful

--- Robots are coming.py
+++ Robots are coming.py
@@ -5,26 +5,42 @@ finished= False
 
 
 def place_player():
-    player_x= randint(0,60) # the X coordinate of the player
-    player_y= randint(0,47) # the y coordinate of the player 
+    player_x= randint(1,60) # the X coordinate of the player
+    player_y= randint(1,47) # the y coordinate of the player 
     c= Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True) 
     print("here i am")
-    player_physical= int(c)
+    player_shape= str(c)
+    
+    key = update_when('key_pressed')
 
+    if key == '6' and player_x < 63:
+        player_x += 1
+    elif key == '3':
+        if player_x < 63:
+            player_x += 1
+        if player_y > 0:
+            player_y -= 1
 
-    def moving_player(): 
+    # You fill in the rest here...
+
+    move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
+()
+
+def moving_player(): 
         while True: 
             x= +4
             y= +3  
-            c= +5 
-            ball_x=Circle((x,y), c)
+            R= +5 
+            ball_x=Circle((x,y), R)
             move_to= ball_x
-            sleep(.02)
+
             if x> 635: break
             else: move_to(ball_x)
+           
             ()       
 
-    place_player()            
-    moving_player()
-    remove_from_screen(c) 
-    end_graphics()
+place_player()            
+moving_player()
+remove_from_screen() 
+sleep(.02)
+end_graphics()
