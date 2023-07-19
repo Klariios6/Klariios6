1fed5859905d8733c5e4fb341d2f25b8738f7b5a
kei larios <katie.melissa345@gmail.com>
2023-07-19 10:58:37 -0400

    changed some things

--- Robots are coming.py
+++ Robots are coming.py
@@ -2,21 +2,20 @@ from gasp import *
 from random import randint
 begin_graphics() 
 finished= False
-player_x=1
-player_y=1 
+player_x= randint(1, 63)
+player_y= randint(1, 47) 
 Robot_1= randint(1,63) # this is for the x 
 robot_2= randint(1,47) # this is for the y 
 
 
 def place_player(): #This is the function that moves the player 
         global player_x, player_y, c
-player_x= randint(1,60) # the X coordinate of the player
-player_y= randint(1,47) # the y coordinate of the player 
 c= Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True) 
 print("here i am")
 ()
+
 def moving_player(): 
-    global player_x, player_y, c, key
+    global player_x, player_y, c
     key= update_when('key_pressed')
     print( 'player is moving')
     while  key == 'z':
@@ -36,11 +35,11 @@ move_to(c,( player_x *10, player_y * 10), 5, filled=True)
 ()
 
 def place_robot():
-     global a, robot_2, Robot_1
+     global a, robot_2, Robot_1 
      a= Circle(( 10* robot_2, 10* Robot_1), 5, filled=True) # Robot entity circle
      ()
 def move_robot(): 
- global robot_2, Robot_1, a
+ global robot_2, Robot_1, a #uses defined a as robot
  if robot_2 > player_y:
       robot_2 -= 1
       if robot_2 < player_y: 
@@ -52,18 +51,18 @@ def move_robot():
                      move_to(a(10* robot_2, Robot_1 *10), 5, filled=True)
 ()
 
-def found_each_other(): 
+def found_each_other(): #when the bots are equal to each other
      global robot_2, Robot_1, player_x, player_y
-     if Robot_1 == player_x:
-          print( 'BOOM')
-          if robot_2 == player_y: 
-               print( 'BOOM ')
-     ()
+     if Robot_1 == player_x: Finished= True
+print( 'BOOM')
+if robot_2 == player_y: finished= True 
+print( 'BOOM ')
+()
 
-     place_robot()
-     move_robot()
-     found_each_other()
+place_robot()
+move_robot()
 place_player()            
-moving_player() 
+moving_player()
+found_each_other() 
 sleep(.02)
 end_graphics()
