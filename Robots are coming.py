16ff6582c350526351a0da0bd676484e804d8b82
kei larios <katie.melissa345@gmail.com>
2023-07-24 10:19:33 -0400

    changed color of the robots

--- Robots are coming.py
+++ Robots are coming.py
@@ -1,64 +1,61 @@
-from gasp import *
-from random import randint  
-player_x= randint(1, 63)
-player_y= randint(1, 47) 
-Robot_1= randint(1,63) # this is for the x 
-robot_2= randint(1,47) # this is for the y 
-
-
-def place_player(): #This is the function that moves the player 
-        global player_x, player_y
-c= Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True, color =color.GREEN) 
-print("here i am")
-()
-
-def moving_player(): 
-    global player_x, player_y, c
-    key= update_when('key_pressed')
-    print( 'player is moving')
-    while  key == 'q':
-          print('bye')
-    remove_from_screen(c)
-    safe_player()
-    if key == "w" and player_y <47: 
-        player_y += 1
-    if key == "s" and player_y > 1: 
-        player_y -= 1
-    if key == "a" and player_x > 1: 
-        player_x -= 1
-    if key == "d" and player_x< 63: 
-        player_x += 1
-
-move_to(c,( player_x *10, player_y * 10), 5, filled=True)
-()
+from gasp import * 
+from random import randint
+player_x = 1
+player_y = 1
+robot_1 = randint(0, 63)
+robot_2 = randint(0, 47)
 
 def place_robot():
-     global a, robot_2, Robot_1 
-     a= Circle(( 10* robot_2, 10* Robot_1), 5, filled=True, color= color.RED) # Robot entity circle
-     ()
-def move_robot(): 
- global robot_2, Robot_1, a #uses defined a as robot
-if robot_2 > player_y:
- robot_2 -= 1
-if robot_2 < player_y: 
- robot_2 += 1
-if Robot_1 > player_x:
- Robot_1 -= 1 
-if Robot_1 < player_x:
- Robot_1 += 1
-move_to(a(10* robot_2, Robot_1 *10), 5, filled=True)
-()
+    global b
+    b = Circle((10 * robot_1 +5 , 10 * robot_2 + 5), 5 , color =color.GREEN)
 
+def move_robot():
+    global robot_1, robot_2, b
+    print('Robot is going after you')
+    if robot_2 > player_y:
+        robot_2 -= 1
+    if robot_2 < player_y:
+        robot_2 += 1
+    if robot_1 > player_x:
+        robot_1 -= 1
+    if robot_1 < player_x:
+        robot_1 += 1
+    move_to(b, (10 * robot_1, 10 * robot_2))
+    
+def place_player():
+    global c, player_x, player_y
+    player_x = randint(0, 63) 
+    player_y = randint(0, 47)
+    c = Circle((10 * player_x + 5 , 10 * player_y + 5), 5, filled = True, color = color.RED)
+
+def move_player():
+    global player_x, player_y, c
+    print('Player is moving')
+    key = update_when('key_pressed')
+    while key == 'q':
+        print('teleport')
+        remove_from_screen(c)
+        safe_player()
+        key = update_when('key_pressed')
+    if key == 'w' and player_y <47:
+        player_y += 1
+    if key == 's' and player_y >1:
+        player_y -=1
+    if key == 'a' and player_x >1:
+        player_x -= 1
+    if key == 'd' and player_x <63:
+        player_x += 1
+    
+    move_to(c, (10 * player_x, 10 * player_y))
 def check_collisions():
     global finished
-    if player_x == Robot_1 and player_y == robot_2:
-        Text("CACHOW!", (320, 240), size=20)
+    if player_x == robot_1 and player_y == robot_2:
+        Text("WHOOPS!", (320, 240), size=20)
         sleep(2)
         finished = True
-
 def safe_player():
     place_player()
-while player_x == Robot_1 and player_y == robot_2:
+while player_x == robot_1 and player_y == robot_2:
     place_player()
 
 begin_graphics()
@@ -67,7 +64,7 @@ finished = False
 safe_player()
 place_robot()
 while not finished:
- moving_player()
- move_robot()
- check_collisions()
+    move_player()
+    move_robot()
+    check_collisions()
 end_graphics()
